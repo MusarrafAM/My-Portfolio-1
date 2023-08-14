f40b325d088bb9cb4284a7d3c862044b4103ffbd
Musarraf <muhammedmusarraf@gmail.com>
2023-08-15 04:17:34 +0530

    Added text selector colours and align navbar a little

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        {[images.react, images.python, images.node].map((circle, index) => (
           <motion.div
             className="circle-cmp app__flex"
             key={`circle-${index}`}
